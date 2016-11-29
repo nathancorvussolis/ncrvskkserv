e2013bb6ffabe0abd7ef32aa803287599c7b67d2
nathancorvussolis <nathancorvussolis@gmail.com>
2016-11-30 02:34:28 +0900

    Removed Google CGI API version

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
 
 * ncrvskkserv.js
 
- * Node.js SKK server (UTF-8)
-
-* ngcrvskkserv.js
-
- * Node.js SKK server using [Google CGI API for Japanese Input](https://www.google.co.jp/ime/cgiapi.html) (UTF-8)
-
-* necrvskkserv.js
-
  * Node.js SKK server
--- ncrvskkserv.js
+++ ncrvskkserv.js
@@ -1,31 +1,25 @@
 /*
-	ncrvskkserv 0.2.2
+	ncrvskkserv 0.3.0
+	Copyright 2013-2016, SASAKI Nobuyuki. Licensed under the MIT license.
 
-	Copyright 2013-2015, SASAKI Nobuyuki. Licensed under the MIT license.
-
-	参照：     http://nodejs.org/
-
-	実行方法： > node.exe ncrvskkserv.js SKK-JISYO-UTF8.L
-
-	備考：     Node.jsを用いたSKK辞書サーバーです。
-	           文字コードがUTF-8、改行がLFのSKK辞書を使用して下さい。
-	           起動時にSKK辞書をメモリ上に展開します。
-	           ポートを1179、文字コードをUTF-8に設定して下さい。
+	Node.js SKK server
+	Usage: node ncrvskkserv.js SKK-JISYO.L
 */
 
 var net = require('net');
 var fs = require('fs');
 var os = require('os');
 
-var port = 1179;
-var version = 'ncrvskkserv 0.2.2';
+var host = '127.0.0.1'
+var port = 1178;
+var version = 'ncrvskkserv 0.3.0';
 var dictionary = new Array();
 
 if(process.argv.length != 3) return;
 
 var server = net.createServer(function(c) {
 	c.on('data', function(data) {
-		var req = data.toString();
+		var req = data.toString('binary');
 		var res = '4\n';
 		var cmd = req.substr(0, 1);
 		switch(cmd) {
@@ -46,41 +40,33 @@ var server = net.createServer(function(c) {
 				else if(key > keycmp) l = m + 1;
 				else if(key < keycmp) r = m - 1;
 			}
-			c.write(res);
+			c.write(res, 'binary');
 			break;
 		case '2':
 			res = version + ' ';
-			c.write(res);
+			c.write(res, 'binary');
 			break;
 		case '3':
 			res = os.hostname() + ':' + c.localAddress + ' ';
-			c.write(res);
+			c.write(res, 'binary');
 			break;
 		default:
-			c.write(res);
+			c.write(res, 'binary');
 			break;
 		}
 	});
 });
 
-server.listen(port, function() {
-	console.log('reading dictionary...');
-	var fc = fs.readFileSync(process.argv[2], 'utf8');
+server.listen(port, host, function() {
+	var fc = fs.readFileSync(process.argv[2], 'binary');
 	var ln = fc.split('\n');
-	var es = -1;
 	for(var i in ln) {
-		if(es == -1) {
-			if(ln[i] == ';; okuri-ari entries.' ||
-				ln[i] == ';; okuri-nasi entries.') es = 0;
-			continue;
-		}
-		var sp = ln[i].indexOf(' ');
+		if(ln[i].startsWith(';;')) continue;
+		var sp = ln[i].indexOf(' /');
 		if(sp == -1) continue;
-		var sl = ln[i].indexOf('/', sp + 1);
-		if(sl == -1) continue;
 		dictionary.push({
 			'key': ln[i].substr(0, sp + 1),
-			'candidate': ln[i].substr(sl) + '\n'
+			'candidate': ln[i].substr(sp + 1) + '\n'
 		});
 	}
 	dictionary.sort(function(x, y) {
